92308f1e0b630aab55ec9498048ff79ed7268bf7
jasenmichael <jasen@ready-builder.com>
2020-03-02 15:56:14 -0500

    remove strapi quick install

--- bin/install-strapi.sh
+++ bin/install-strapi.sh
@@ -1,6 +1,6 @@
 echo "installing Strapi in $1/$2"
 cd $1
-npx create-strapi-app $2 --no-run --use-$3 --quickstart
+npx create-strapi-app $2 --no-run --use-$3
 echo "Strapi installed"
 echo ===================================
 
