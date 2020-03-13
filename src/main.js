fde7971e1aa575c7a23e51ef88ae69572ecbde43
jasenmichael <jasen@ready-builder.com>
2020-03-12 20:55:24 -0400

    fix install nuxt

--- src/main.js
+++ src/main.js
@@ -15,10 +15,10 @@ const chalk = require('chalk')
 const logSymbols = require('log-symbols');
 
 // install Strapux, assumes from cli
-async function install(path, options) {
+async function install(path, options = false) {
     // const path = args[0]
-    console.log('path-------', path)
-    options = JSON.parse(options)
+    console.log('path-----------=====---', path)
+    options = options ? JSON.parse(options) : {}
     // console.log(options)
     // we did mkdir, npm init, npm i stapux via create-strapux-app, or manually
     const tasks = new Listr([{
